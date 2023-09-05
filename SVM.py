83c763a94e7e77902f16da7b1397866df049f2a7
prathamesh palatshaha <prathamesh.palatshaha@vit.edu.in>
2023-09-05 20:27:18 +0530

    05-09-2023

--- SVM.py
+++ SVM.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-class SVM():
-    def __init__(self, iteration, rate, l):
+class SVM_classifier:
+    def __init__(self, iteration, rate, lamb):
         self.iteration = iteration
         self.rate = rate
-        self.l = l
+        self.lamb = lamb
 
     def fit(self, X, Y):
         self.rows, self.features = X.shape
@@ -13,35 +13,28 @@ class SVM():
         self.cost = 0
         self.X = X
         self.Y = Y
-
+        # self.y_cap = np.where(self.Y <= 0, -1, 1)
         for i in range(self.iteration):
             self.gradient_descent()
 
     def gradient_descent(self):
         y_label = np.where(self.Y <= 0, -1, 1)
 
-        # gradients ( dw, db)
-        for index, x_i in enumerate(self.X):
-
-            condition = y_label[index] * (np.dot(x_i, self.weight) - self.cost) >= 1
-
-            if (condition == True):
-
+        for idx, value in enumerate(self.X):
+            if (np.dot(y_label[idx], (np.dot(self.weight, value) - self.cost)) >= 1).any():
                 dw = 2 * self.lamb * self.weight
                 db = 0
-
             else:
-
-                dw = 2 * self.lamb * self.weight - np.dot(x_i, y_label[index])
-                db = y_label[index]
+                dw = 2 * self.lamb * self.weight - np.dot(y_label[idx], value)
+                db = y_label[idx]
 
             self.weight = self.weight - self.rate * dw
 
             self.cost = self.cost - self.rate * db
 
-    def predict(self,X):
+    def predict(self, X):
         op = np.dot(X, self.weight) - self.cost
         pred_lables = np.sign(op)
-        y_cap = np.where(pred_lables <=-1, 0, 1)
+        y_cap = np.where(pred_lables <= -1, 0, 1)
 
         return y_cap
